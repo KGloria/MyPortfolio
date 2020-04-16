4623d3eac03ffbe1ac3ab4e3146998b0ef6325ae
Kyle S Gloria <kgcodemail@gmail.com>
2020-04-16 01:26:48 -0700

    Created deployment build

---
+++ myportfolio/build/precache-manifest.2096100d7588c8efd944fe39602150ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185bdc6e2003b2ef7318188adfce30c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e6d748ca2bd11fea65",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "ae4390bba1338a22fbb5",
+    "url": "/static/css/main.197a70b0.chunk.css"
+  },
+  {
+    "revision": "c0e6d748ca2bd11fea65",
+    "url": "/static/js/2.d6a4a092.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/static/js/2.d6a4a092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4390bba1338a22fbb5",
+    "url": "/static/js/main.d3bfe21c.chunk.js"
+  },
+  {
+    "revision": "76ba2de154fd6435eca6",
+    "url": "/static/js/runtime-main.a20641ec.js"
+  }
+]);
